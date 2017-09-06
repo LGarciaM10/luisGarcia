316e5048882770c4b7dfc4abc0cab4656f924672
Luis Fernando Garcia Miranda <luis.garcia10@flex.com>
2017-09-06 12:27:38 -0700

    [Studio] Modified screen "viewissues"

--- client/controllers/app/viewissues.js
+++ client/controllers/app/viewissues.js
@@ -2,8 +2,4 @@ angular.module('app').controller('app_viewissues', app_viewissues);
 function app_viewissues($scope, app) {
     'use strict';
     app.init($scope);
-    
-    $scope.reportIssue_fnc = function() {
-        
-    }
 }
