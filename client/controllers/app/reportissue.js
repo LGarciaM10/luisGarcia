33c3e28d0b0f650517e61230b3d101fea5843959
Luis Fernando Garcia Miranda <luis.garcia10@flex.com>
2017-09-06 11:49:46 -0700

    [Studio] Modified screen "reportissue"

--- client/controllers/app/reportissue.js
+++ client/controllers/app/reportissue.js
@@ -2,4 +2,8 @@ angular.module('app').controller('app_reportissue', app_reportissue);
 function app_reportissue($scope, app) {
     'use strict';
     app.init($scope);
+    
+    $scope.reportissuefnc = function() {
+        app.action('reportissue', 'submit', this)
+    }
 }
